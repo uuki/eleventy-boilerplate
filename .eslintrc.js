91773d89a4813e1d2cea78334fe05b3e7598dd46
uuki <uuki.dev@gmail.com>
2021-03-04 02:45:48 +0900

    modify babel environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     node: true,
     es2020: true,
   },
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
